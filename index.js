36dd39db7107dd2b7c203458668b9474adb1ca15
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 15:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,4 +52,8 @@ class Delivery {
   meal() {
     return store.meals.find(meal => meal.id === this.mealId)
   }
+
+  customer() {
+    return store.customers.find(customer => customer.id === this.customerId)
+  }
 }

58f06a467bdd899243c57476283d76ed2ddbcb7c
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 14:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Neighborhood {
 
   deliveries() {
     return store.deliveries.filter(delivery =>
-    delivery.neighborhoodId === this.id)
+    delivery.neighborhoodId == this.id)
   }
 }
 

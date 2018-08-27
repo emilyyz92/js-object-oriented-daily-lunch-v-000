3aabb9507c950f1c658708cf7f701d588b9cded2
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 14:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
-let neighborId = 0;
+let neighborhoodId = 0;
 let customerId = 0;
 let mealId = 0;
 let deliveryId = 0;

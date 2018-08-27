602b01505722d575b75afaa5da46fc34fcaa5069
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 14:29:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborId = 0;
+
 class Neighborhood {
   constructor(name) {
     this.name = name
+    this.id = neighborId++
   }
 }
